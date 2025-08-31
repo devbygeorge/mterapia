9b76a1010e88e69902473fb58d6451e1d3660f7b
devbygeorge <gpasiesh@gmail.com>
2025-08-31 07:34:22 +0400

    fix scaling issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>დავით ბალხამიშვილი - მანუალური თერაპია</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
       </Head>
       <div className={firaGO.className}>
         <Component {...pageProps} />
